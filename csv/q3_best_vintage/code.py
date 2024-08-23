fe125e611b28c88af7d9fc294dc0dea95dbb7075
Atome1212 <col.gregoire@gmail.com>
2024-08-23 11:24:29 +0200

    Create code.py

---
+++ csv/q3_best_vintage/code.py
@@ -0,0 +1,31 @@
+import sqlite3
+import csv
+
+conn = sqlite3.connect('db/vivino.db')
+cursor = conn.cursor()
+
+    req = ('''
+           select vintages.name, vintages.year, vintages.price_euros, vintages.ratings_average, countries.name as country
+           from vintages
+           join wines on vintages.wine_id = wines.id
+           join regions on wines.region_id = regions.id
+           join countries on regions.country_code = countries.code
+           where vintages.ratings_count > 500
+           order by vintages.ratings_average desc
+           limit 1
+    ''')
+
+cursor.execute(req)
+
+rows = cursor.fetchall()
+csv_file_name = 'wines_data.csv'
+
+with open(csv_file_name, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    
+    headers = [i[0] for i in cursor.description]
+    csv_writer.writerow(headers)
+    
+    csv_writer.writerows(rows)
+
+conn.close()
