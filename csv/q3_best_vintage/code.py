bddb2f706d0cd9d410a977ed8f765afaa8f4a514
Atome1212 <col.gregoire@gmail.com>
2024-08-23 11:38:12 +0200

    Update code.py

--- csv/q3_best_vintage/code.py
+++ csv/q3_best_vintage/code.py
@@ -4,16 +4,16 @@ import csv
 conn = sqlite3.connect('db/vivino.db')
 cursor = conn.cursor()
 
-    req = ('''
-           select vintages.name, vintages.year, vintages.price_euros, vintages.ratings_average, countries.name as country
-           from vintages
-           join wines on vintages.wine_id = wines.id
-           join regions on wines.region_id = regions.id
-           join countries on regions.country_code = countries.code
-           where vintages.ratings_count > 500
-           order by vintages.ratings_average desc
-           limit 1
-    ''')
+req = ('''
+       select vintages.name, vintages.year, vintages.price_euros, vintages.ratings_average, countries.name as country
+       from vintages
+       join wines on vintages.wine_id = wines.id
+       join regions on wines.region_id = regions.id
+       join countries on regions.country_code = countries.code
+       where vintages.ratings_count > 500
+       order by vintages.ratings_average desc
+       limit 1
+''')
 
 cursor.execute(req)
 
