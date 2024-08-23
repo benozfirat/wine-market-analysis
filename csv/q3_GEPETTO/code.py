62a5ebfb84fa71d087843206f3211c115e996675
Atome1212 <col.gregoire@gmail.com>
2024-08-23 11:37:56 +0200

    Create code.py

---
+++ csv/q3_GEPETTO/code.py
@@ -0,0 +1,30 @@
+import sqlite3
+import csv
+
+conn = sqlite3.connect('db/vivino.db')
+cursor = conn.cursor()
+
+req = ('''
+    select wines.name as name, wines.ratings_average as  "rating avg",  wines.ratings_count as "rating count", countries.name as country
+    from wines 
+    join regions on wines.region_id = regions.id
+    join countries on regions.country_code = countries.code
+    where countries.name = 'Italie'
+    order by wines.ratings_average desc
+    limit 1
+       ''')
+
+cursor.execute(req)
+
+rows = cursor.fetchall()
+csv_file_name = 'wines_data.csv'
+
+with open(csv_file_name, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    
+    headers = [i[0] for i in cursor.description]
+    csv_writer.writerow(headers)
+    
+    csv_writer.writerows(rows)
+
+conn.close()
