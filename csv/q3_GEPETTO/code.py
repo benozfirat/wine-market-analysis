477213f78d3e8216c8aa88beacf77fe18bd9b7dc
Atome1212 <col.gregoire@gmail.com>
2024-08-26 16:15:38 +0200

    Update code.py

--- csv/q3_GEPETTO/code.py
+++ csv/q3_GEPETTO/code.py
@@ -6,11 +6,11 @@ cursor = conn.cursor()
 
 req = ('''
         SELECT 
-            wines.name AS Wines, 
+            wines.name AS "wines_name", 
             countries.name AS country,
-            SUM(wines.ratings_count) AS "wines rating count",
-            AVG(vintages.ratings_average) AS "vintages rating avg",
-            AVG(wines.ratings_average) AS "wines rating avg"
+            SUM(wines.ratings_count) AS "wines_rating_count",
+            AVG(vintages.ratings_average) AS "vintages_rating_avg",
+            AVG(wines.ratings_average) AS "wines_rating_avg"
             
         FROM 
             wines 
