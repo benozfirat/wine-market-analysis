9aa04fa7d5267e4547918ba42cac5cefe5b2e215
Atome1212 <col.gregoire@gmail.com>
2024-08-26 15:52:18 +0200

    Update code.py

--- csv/q3_best_vintage/code.py
+++ csv/q3_best_vintage/code.py
@@ -14,6 +14,7 @@ req = ('''
             SUM(wines.ratings_count) AS "Wines rating count",
             SUM(vintages.ratings_count) AS total_ratings_count,
             AVG(vintages.ratings_average) AS avg_ratings
+            SUM(vintages.rating_count) as "Vintage rating count"
         FROM vintages
         JOIN wines ON vintages.wine_id = wines.id
         JOIN regions ON wines.region_id = regions.id
